5c209889704096c1415ea03f74862b3d50a6e457
Adrian May <adrian.alexander.may@gmail.com>
2017-04-20 18:05:15 +0100

    Types in place

--- Checkout.scala
+++ Checkout.scala
@@ -1,4 +1,21 @@
 object Checkout {
+
+	// Prices all in pence in this program
+
+	type Pence = Int
+	type Item = String
+	type Shopping = List[Item]
+
+	//Prices of apples and oranges:
+	val pa:Pence = 60
+	val po:Pence = 25
+
+	//Shopping list evaluator:
+	def priceOfShopping(shopping : Shopping): Int = 0
+
+	//Test cases:
+	val cases : List[(Pence, Shopping)] = Nil
+
 	def main(args : Array[String]) : Unit = {
 		println("Hello, world")
 	}
